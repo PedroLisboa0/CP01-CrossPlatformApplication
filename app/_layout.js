17fd060df70facbf16242eab144dd14aac9a2fdb
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-19 20:24:47 -0300

    feat: protótipo Header

--- app/_layout.js
+++ app/_layout.js
@@ -1,20 +1,50 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#E83D84' }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#E83D84',
+
+        // HEADER GLOBAL
+        headerStyle: {
+          backgroundColor: '#fff',
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 20,
+        },
+        headerShadowVisible: false, // remove sombra feia padrão
+
+        // ÍCONE DIREITA (engrenagem)
+        headerRight: () => (
+          <Ionicons
+            name="settings-outline"
+            size={24}
+            style={{ marginRight: 15 }}
+            color="#000"
+          />
+        ),
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
         }}
       />
+
       <Tabs.Screen
         name="perfil"
         options={{
           title: 'Loja',
-          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
