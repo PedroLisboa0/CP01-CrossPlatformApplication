c6ac5ec7bdfcd50eb7fa9af9cac3aab14738056f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-19 19:38:59 -0300

    feat: projeto base

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <View>
+
+
+    </View>
+  );
+}
---
+++ app/sobre.js
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Perfil() {
+  const router = useRouter();
+
+  return (
+    <View>
+
+
+
+    </View>
+  );
+}
+
+// Css da responsividade desse componente
+// Estilização removida conforme solicitado.
